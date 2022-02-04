d5390758b13422f508383e22dae25b5738485afc
mhw0 <mhw0@yahoo.com>
2022-02-04 15:39:42 +0500

    errors: create `@theta-rpc/errors`

--- packages/say_hi/src/index.ts
+++
@@ -1,3 +0,0 @@
-export function getMsg() {
-  return 'hi';
-}
--- packages/say_hi/test/say_hi.ts
+++
@@ -1,7 +0,0 @@
-import test from 'tape';
-import { getMsg } from '../src';
-
-test('say_hi', ({ equal, end }) => {
-  equal(getMsg(), 'hi');
-  end();
-});
